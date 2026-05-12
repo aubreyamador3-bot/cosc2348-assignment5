b7616cb31232c01e85945228f278732a6352431a
audreyamador02 <audreyamador@icloud.com>
2026-05-12 05:28:33 -0500

    Fix your_tests.py to use correct format

--- calculator/tests/your_tests.py
+++ calculator/tests/your_tests.py
@@ -1,10 +1,12 @@
 #!/usr/bin/env python3
 from calculator_adapter import run
 
-
 ### ADD AT LEAST TWO TESTS HERE!
 
+# Checks that the program outputs "8" for an input of "3 + 5"
+assert run("3 + 5").output == "8"
 
-###
+# Checks that the program outputs "6" for an input of "10 - 4"
+assert run("10 - 4").output == "6"
 
 print("All tests passed!")
